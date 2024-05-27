116be643b6c18c29628a3af85aa42f52f9e2066a
coderwhytop <1795691637@qq.com>
2024-05-27 08:18:42 +0800

    修复main.mjs的不存在命令时提示

--- src/exec/config.mjs
+++ src/exec/config.mjs
@@ -1,7 +1,18 @@
 import Storage from "../utils/Storage.mjs";
-
+import Commands from "../common/Commands.mjs";
 const storage = new Storage()
-
+const commands = new Commands()
+class Config{
+  constructor() {
+    this.command = 'config'
+  }
+  init(args){
+    this.args = args
+  }
+  error(overflow){
+    const complete = '"' + commands.main + this.command + ' ' + args.join(' ') + '"'
+  }
+}
 async function config(args){
   const plugins = storage.getPlugins()
   const installs = storage.getInstalls()
@@ -10,4 +21,5 @@ async function config(args){
   const action = args
 }
 
-export default config
+const c = new Config()
+export default c.init
